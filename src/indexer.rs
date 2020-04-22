a3e64e617b28b112c3db9fc39046d8cd6bc943e4
Sylvester Chin <sylvester.csw@gmail.com>
2020-04-22 22:30:24 +0800

    fix(add): handle alias clashing (#17)
    Aliases are typically the folder name, but identical folders with
    different paths are common. This PR performs reverse comparison to find
    the shortest distinct subpath between 2 identical folders.
    
    Closes issue #15.

--- src/indexer.rs
+++ src/indexer.rs
@@ -1,9 +1,10 @@
 use crate::db;
 use anyhow::Result;
+use itertools::{EitherOrBoth::*, Itertools};
 use std::collections::HashMap;
 use std::env;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 pub fn update(level: i8) -> Result<()> {
     let mut db = db::read_db()?;
@@ -17,15 +18,7 @@ pub fn update(level: i8) -> Result<()> {
             let p = cur.pop().unwrap();
 
             // set immediate folder name as alias
-            db.insert(
-                p.display()
-                    .to_string()
-                    .split("/")
-                    .last()
-                    .unwrap()
-                    .to_string(),
-                p.display().to_string(),
-            );
+            insert(&mut db, &p);
 
             // push sub-directories into next
             if p.is_dir() {
@@ -54,4 +47,75 @@ pub fn prune() -> Result<()> {
 
     db::write_db(pruned)?;
     Ok(())
+}
+
+// insert handles the logic for inserting a new path
+// if another folder exists in the db with a different absolute path,
+// `get_shortest_distinct_path` will generate the non-clashing alias pair.
+// TODO settle mutable borrow warnings
+fn insert(db: &mut HashMap<String, String>, p: &PathBuf) {
+    let v = p.display().to_string();
+    let k = v.split("/").last().unwrap().to_string();
+
+    match db.get(&k) {
+        None => db.insert(k, v),
+        Some(v_existing) => {
+            if *v_existing != p.display().to_string() {
+                let (existing, clashing) =
+                    get_shortest_distinct_paths(&mut v.clone(), &mut String::from(v_existing));
+                db.insert(existing, v_existing.to_string());
+                db.insert(clashing, v);
+                db.remove(&k);
+            }
+            None
+        }
+    };
+    ()
+}
+
+fn get_shortest_distinct_paths(a: &mut String, b: &mut String) -> (String, String) {
+    let asplit = a.split("/").collect::<Vec<&str>>();
+    let bsplit = b.split("/").collect::<Vec<&str>>();
+    let mut avec = Vec::new();
+    let mut bvec = Vec::new();
+
+    for pair in asplit.iter().rev().zip_longest(bsplit.iter().rev()) {
+        match pair {
+            Both(l, r) => {
+                avec.push(*l);
+                bvec.push(*r);
+                if l != r {
+                    break;
+                }
+            }
+            Left(l) => {
+                avec.push(l);
+                break;
+            }
+
+            Right(r) => {
+                bvec.push(r);
+                break;
+            }
+        }
+    }
+
+    avec.reverse();
+    bvec.reverse();
+    (avec.join("/"), bvec.join("/"))
+}
+
+#[test]
+fn test_diff_length() {
+    let (a, b) = get_shortest_distinct_paths(&mut String::from("a/b/c"), &mut String::from("c"));
+    assert_eq!(a, "b/c");
+    assert_eq!(b, "c");
+}
+
+#[test]
+fn test_same_length() {
+    let (a, b) =
+        get_shortest_distinct_paths(&mut String::from("a/b/c"), &mut String::from("a/a/c"));
+    assert_eq!(a, "b/c");
+    assert_eq!(b, "a/c");
 }
